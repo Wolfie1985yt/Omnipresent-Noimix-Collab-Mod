00e3ca14901d0e62da7c20745d230900fdab13a7
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2024-10-27 16:07:01 +0200

    the mobile setup

--- setup/setup-android.bat
+++ setup/setup-android.bat
@@ -3,17 +3,20 @@ color 0a
 cd ..
 @echo on
 echo Installing dependencies.
-haxelib install lime
-haxelib install openfl
-haxelib install flixel
-haxelib install flixel-addons
+haxelib install lime 8.0.2
+haxelib set lime 8.0.2
+haxelib install openfl 9.2.2
+haxelib set openfl 9.2.2
+haxelib install flixel 5.5.0
+haxelib set flixel 5.5.0
+haxelib install flixel-addons 3.0.2
+haxelib set flixel-addons 3.0.2
 haxelib install flixel-ui
 haxelib install flixel-tools
 haxelib set hxCodec 2.6.1
 haxelib install tjson
-haxelib set SScript 8.1.6
-haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
-haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
-haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc
+haxelib git SScript https://github.com/MaysLastPlays-Stuff/SScript
+haxelib git flxanimate https://github.com/MaysLastPlays-Stuff/flxanimate dev
+haxelib git linc_luajhaxelib set flixelit https://github.com/superpowers04/linc_luajit
 echo Finished!
 pause
