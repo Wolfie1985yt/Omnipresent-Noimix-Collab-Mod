6e4fe28bfc7593e0dc920774564efcac6da07b39
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2024-11-05 09:52:51 +0200

    new test

--- source/options/BaseOptionsMenu.hx
+++ source/options/BaseOptionsMenu.hx
@@ -129,9 +129,9 @@ class BaseOptionsMenu extends MusicBeatSubstate
 		var pressedEnter:Bool = FlxG.keys.justPressed.ENTER || controls.ACCEPT;
 		for (touch in FlxG.touches.list)
 		{
-			if (touch.justPressed)
+			if (FlxG.touches.list.length >= 2 &&  && FlxG.touches.list[0].pressed && FlxG.touches.list[1].pressed)
 			{
-				pressedEnter = false;
+				pressedEnter = true;
 			}
 		}
 		#end
