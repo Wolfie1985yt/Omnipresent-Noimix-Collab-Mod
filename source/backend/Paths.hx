ad4e1d394ce6f7a1112581e22e594134bca4ebf5
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2024-11-06 06:49:45 +0200

    update source/backend/Paths.hx

--- source/backend/Paths.hx
+++ source/backend/Paths.hx
@@ -304,7 +304,7 @@ class Paths
 
 	static public function getTextFromFile(key:String, ?ignoreMods:Bool = false):String
 	{
-		#if sys
+		#if desktop
 		#if MODS_ALLOWED
 		if (!ignoreMods && FileSystem.exists(modFolders(key)))
 			return File.getContent(modFolders(key));
